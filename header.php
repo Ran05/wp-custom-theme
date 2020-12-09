f28c20cc1898ffb0c04f02ca2ea53306fc3c3f51
Randolfh <ferarizarandolfh.ifliba@gmail.com>
2020-12-08 22:55:56 -0800

    add more comments

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="<?= language_attributes();?>">
   <head>
+    <!-- sample blog post -->
     <meta charset="<?= bloginfo('charset');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="<?= bloginfo('description');?>">
