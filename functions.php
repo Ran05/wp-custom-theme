8a590769f8072405b9385ffaad8841bd9317be84
Randolfh <ferarizarandolfh.ifliba@gmail.com>
2020-12-11 03:50:45 -0800

    wp excerpt length

--- functions.php
+++ functions.php
@@ -17,8 +17,17 @@ function wp_custom_list_class($atts, $items, $args){
 
 }
 
+//add a function for post length
+
+function set_the_length() {
+    return 35;
+}
+
+
+
 // web hooks
 add_action('after_setup_theme', 'wpb_theme_setup'); //call back
 add_filter('nav_menu_link_attributes', 'wp_custom_list_class', 10 , 3);
+add_filter('excerpt_length','set_the_length' ); // web hooks for excerpt length
 
 ?>
