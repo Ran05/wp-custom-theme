74676b064254ef8f6e645ea65cbe5f0369397100
Randolfh <ferarizarandolfh.ifliba@gmail.com>
2020-12-18 20:16:42 -0800

    add-featured image to post

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 # here is all functionsgi
 // add nav menus
 function wpb_theme_setup(){
-
+    add_theme_support('post-thumbnails'); // added featured image here
     register_nav_menus(array(
             'primary'=> __('Primary Menu')
 
